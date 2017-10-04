e859758ad58ea91a5445e8546af98f2d90d17091
Vinícius Ribeiro <vy_ribeiro@yahoo.com.br>
2017-10-03 23:48:04 -0300

    Commit Inicial

---
+++ backend/src/main/java/br/com/programamexase/mexase/MexaseApplication.java
@@ -0,0 +1,12 @@
+package br.com.programamexase.mexase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MexaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MexaseApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/br/com/programamexase/mexase/MexaseApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.programamexase.mexase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MexaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
